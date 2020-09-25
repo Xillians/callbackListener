f735311fbb7d64f16847421da979f4fb5422636c
Xillians <chkxillians@gmail.com>
2020-09-25 14:33:11 +0200

    Added index as a landing page. Also added writing to file logic.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const path = require('path');
+const fs = require('fs');
 const app = express();
 dotenv.config();
 
@@ -9,7 +11,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/", (req,res) => {
     console.log("Request body:\n", req.body);
+    const requestBody = JSON.stringify(req.body);
+    fs.appendFile("./data/callbacks.txt", requestBody,  (err)=> {
+        if(err)
+            return console.log(err);
+    });
     return res.end();
+});
+
+app.get("/", (req, res)=> {
+    res.sendFile(path.join(__dirname + '../../index.html'));
 });
 
 const server = app.listen(process.env.PORT, () => {
