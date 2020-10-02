bb8a0870548df9c38c35bd603b8d9e3474296fcc
Xillians <chkxillians@gmail.com>
2020-10-02 15:46:03 +0200

    Improved error handling on file not found.

--- src/controllers/filesystem.ts
+++ src/controllers/filesystem.ts
@@ -7,33 +7,38 @@ export class FileSystem {
     }
     public writeToFile(req: any) {
         const requestBody = JSON.stringify(req.body);
-        fs.appendFile("./data/callbacks.txt", requestBody,  (err)=> {
-            if(err)
+        fs.appendFile("./data/callbacks.txt", requestBody, (err) => {
+            if (err)
                 return console.log(err);
         });
-        fs.appendFile("./data/callbacks.txt", "\n",  (err)=> {
-            if(err)
+        fs.appendFile("./data/callbacks.txt", "\n", (err) => {
+            if (err)
                 return console.log(err);
         });
     }
-    
+
     public readFromFile() {
         return fs.readFileSync("./data/callbacks.txt", "utf8");
     }
 
     public createFile(callbackId: string, req: any) {
         const requestBody = JSON.stringify(req.body);
-        fs.appendFile(`./data/${callbackId}-callbacks.txt`, requestBody,  (err)=> {
-            if(err)
+        fs.appendFile(`./data/${callbackId}-callbacks.txt`, requestBody, (err) => {
+            if (err)
                 return console.log(err);
         });
-        fs.appendFile(`./data/${callbackId}-callbacks.txt`, "\n",  (err)=> {
-            if(err)
+        fs.appendFile(`./data/${callbackId}-callbacks.txt`, "\n", (err) => {
+            if (err)
                 return console.log(err);
         });
     }
 
     public readCallback(callbackId: string) {
-        return fs.readFileSync(`./data/${callbackId}-callbacks.txt`, "utf8");
+        try {
+            return fs.readFileSync(`./data/${callbackId}-callbacks.txt`, "utf8");
+        } catch (error) {
+            console.log(error.message);
+            return {"error": `ID: ${callbackId} not found`};
+        }
     }
 }
